07f7e172738081827f7bc70f2b15770ffb7106d9
cpp-maker <t.frankel@outlook.com>
2020-10-17 20:49:46 +0100

    finished task input widget, mostly bug free but checkbox becomes unresponsive for 2 clicks after being checked, tasks can now be added by user

--- src/protool/Task.py
+++ src/protool/Task.py
@@ -1,6 +1,7 @@
 # This Python file uses the following encoding: utf-8
 from PySide2.QtWidgets import QListWidgetItem, QListWidget, QCheckBox, QWidget, QHBoxLayout, QFormLayout, QLabel, QSpinBox, QLineEdit
-from PySide2.QtGui import QColor, QBrush, QLinearGradient
+from PySide2.QtGui import QColor, QBrush, QLinearGradient, QPalette
+from PySide2.QtCore import Signal, Slot
 from datetime import datetime
 
 
@@ -56,9 +57,9 @@ class TaskWidget(QListWidgetItem):
 class ToDoList(QListWidget):
     def __init__(self,tasks,*args,**kwargs):
         super(ToDoList,self).__init__(*args,**kwargs)
-        tasks = self.sortTasks(tasks)
+        self.tasks = self.sortTasks(tasks)
 
-        for task in tasks:
+        for task in self.tasks:
             self.addItem(TaskWidget(task))
 
         for i in range(self.count()):
@@ -70,19 +71,53 @@ class ToDoList(QListWidget):
         tasks.sort(key=lambda t: t.deadline)
         return tasks
 
+    @Slot(Task)
+    def addTask(self,task):
+        if task.name == "" or task.description == "":
+            return
+
+        self.tasks.append(task)
+
+        for j in range(3):
+            for i in range(self.count()):
+                item = self.takeItem(i)
+
+        self.tasks = self.sortTasks(self.tasks)
+
+        for task in self.tasks:
+            self.addItem(TaskWidget(task))
+
+        for i in range(self.count()):
+            self.setItemWidget(self.item(i), QCheckBox())
+
 class TaskInputFieldWidget(QWidget):
+    add = Signal(Task)
+
     def __init__(self,*args,**kwargs):
         super(TaskInputFieldWidget,self).__init__(*args,**kwargs)
 
-        self.layout = QFormLayout(self)
+        self.setForegroundRole(QPalette.Text)
+
+        palette = QPalette()
+        palette.setBrush(QPalette.Text,QBrush(QColor.fromRgbF(0.9,0.9,0.9,1)))
+        self.mainlayout = QHBoxLayout(self)
+
+        self.layout = QFormLayout()
         self.deadlineInputLayout = QHBoxLayout()
         self.deadlineInputYear = QSpinBox()
         self.deadlineInputMonth = QSpinBox()
         self.deadlineInputDay = QSpinBox()
 
-        self.deadlineLabelYear = QLabel("YYYY")
-        self.deadlineLabelMonth = QLabel("MM")
-        self.deadlineLabelDay = QLabel("DD")
+        self.deadlineInputYear.setMaximum(3000)
+        self.deadlineInputYear.setMinimum(2020)
+        self.deadlineInputMonth.setMaximum(12)
+        self.deadlineInputMonth.setMinimum(1)
+        self.deadlineInputDay.setMaximum(31)
+        self.deadlineInputDay.setMinimum(1)
+
+        self.deadlineLabelYear = QLabel("<font color=white>YYYY</font>")
+        self.deadlineLabelMonth = QLabel("<font color=white>MM</font>")
+        self.deadlineLabelDay = QLabel("<font color=white>DD</font>")
 
         self.deadlineInputLayout.addWidget(self.deadlineLabelYear)
         self.deadlineInputLayout.addWidget(self.deadlineInputYear)
@@ -94,17 +129,34 @@ class TaskInputFieldWidget(QWidget):
         self.nameInput = QLineEdit()
         self.descriptionInput = QLineEdit()
 
-        self.nameLabel = QLabel("Name:")
-        self.descriptionLabel = QLabel("Description:")
-        self.deadlineLabel = QLabel("Deadline:")
+        self.nameLabel = QLabel("<font color=white>Name:</font>")
+        self.descriptionLabel = QLabel("<font color=white>Description:</font>")
+        self.deadlineLabel = QLabel("<font color=white>Deadline:</font>")
 
         self.layout.addRow(self.nameLabel,self.nameInput)
         self.layout.addRow(self.descriptionLabel,self.descriptionInput)
         self.layout.addRow(self.deadlineLabel,self.deadlineInputLayout)
 
-        self.setLayout(self.layout)
+        self.done = QCheckBox()
+        self.mainlayout.addWidget(self.done)
+        self.mainlayout.addLayout(self.layout)
+        self.setLayout(self.mainlayout)
 
         self.show()
 
-    def createNewTask(self):
-        pass
+    def inputToDate(self):
+        return datetime(self.deadlineInputYear.value(),self.deadlineInputMonth.value(),self.deadlineInputDay.value())
+
+    def createTask(self):
+        if self.done.checkState() == False:
+            return
+
+        self.done.setChecked(False)
+        task = Task(self.inputToDate(),self.nameInput.text(),self.descriptionInput.text())
+        self.nameInput.clear()
+        self.descriptionInput.clear()
+        self.deadlineInputYear.setValue(datetime.now().year)
+        self.deadlineInputMonth.setValue(datetime.now().month)
+        self.deadlineInputDay.setValue(datetime.now().day)
+
+        self.add.emit(task)
