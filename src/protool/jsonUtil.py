a54e73883cb869e73db828b249833f3618fb215d
cpp-maker <t.frankel@outlook.com>
2020-10-30 11:38:57 +0000

    all server functionality successfully integrated bug free

--- src/protool/jsonUtil.py
+++ src/protool/jsonUtil.py
@@ -6,10 +6,7 @@ import curriculum
 import datetime
 import requests
 
-#must read in all of the json
-#must read in all tasks
 #if there is time create error handling for erroneous json data
-#create window to initialise new user
 #work on curriculums
 
 def stringToDatetime(string):
@@ -22,44 +19,49 @@ def readAll():
         fileContents = file.read()
         dictionary = json.loads(fileContents)
 
-    user.user = user.User(dictionary["name"],0,dictionary["task_completion_rate"],dictionary["missed_deadline"],dictionary["weekly_productivity_score"],dictionary["weekly_task_completion_rate"],dictionary["weekly_deadlines_missed"],dictionary["id"],dictionary["email"],dictionary["pin"])
+    user.user = user.User(dictionary["name"],dictionary["productivity_score"],dictionary["task_completion_rate"],dictionary["missed_deadline"],dictionary["weekly_productivity_score"],dictionary["weekly_task_completion_rate"],dictionary["weekly_deadlines_missed"],dictionary["id"],dictionary["email"],dictionary["pin"])
 
-    if dictionary["tasks"][0] == None:
-        task.tasks = [None]
+    if len(dictionary["tasks"]) == 0:
+        task.tasks = []
     else:
         task.tasks = [task.Task(name=tempDict["name"],description=tempDict["description"],deadline=stringToDatetime(tempDict["deadline"])) for tempDict in dictionary["tasks"]]
 
-    if dictionary["flashcards"][0] == None:
-        flash.flashcards = [None]
+    if len(dictionary["flashcards"]) == 0:
+        flash.flashcards = []
     else:
         flash.flashcards = [flash.FlashCard(tempDict["subject"],tempDict["front_text"],tempDict["back_text"]) for tempDict in dictionary["flashcards"]]
 
-    if dictionary["curriculums"][0] == None:
-        curriculum.curriculums = [None]
+    if len(dictionary["curriculums"]) == 0:
+        curriculum.curriculums = []
     else:
         curriculum.curriculums = [curriculum.Curriculum(tempDict["name"],tempDict["subject"],tempDict["topics"]) for tempDict in dictionary["curriculums"]]
 
 def writeAll(user,curriculums,tasks,flashcards): #create case to write null in lists if any of the arrays are empty
-    jsonString = user.__dict__()
-    curriculumsString = [None]
-    tasksString = [None]
-    flashcardsString = [None]
-
-    if len(curriculum.curriculums) != 0 and curriculum.curriculums[0] != None:
-        curriculumsString = [curriculum.__dict__() for curriculum in curriculums]
-
-    if len(task.tasks) != 0 and task.tasks[0] != None:
-        tasksString = [task.__dict__() for task in tasks]
-
-    if len(flash.flashcards) != 0 and flash.flashcards[0] != None:
-        flashcardsString = [flashcard.__dict__() for flashcard in flashcards]
-
-    jsonString["tasks"] = tasksString
-    jsonString["flashcards"] = flashcardsString
-    jsonString["curriculums"] = curriculumsString
+    jsonString = toJson(user,curriculums,tasks,flashcards)
 
     with open("data.json",'wt') as file:
         file.write(json.dumps(jsonString))
 
     r = requests.put('http://0.0.0.0:54321/' + str(user.id) + '/' + str(user.pin) + '/',json=jsonString)
     print(r.status_code)
+
+def toJson(user,curriculums,tasks,flashcards):
+    jsonString = user.__dict__()
+    curriculumsString = []
+    tasksString = []
+    flashcardsString = []
+
+    if len(curriculum.curriculums) != 0:
+        curriculumsString = [curriculum.__dict__() for curriculum in curriculums]
+
+    if len(task.tasks) != 0:
+        tasksString = [task.__dict__() for task in tasks]
+
+    if len(flash.flashcards) != 0:
+        flashcardsString = [flashcard.__dict__() for flashcard in flashcards]
+
+    jsonString["tasks"] = tasksString
+    jsonString["flashcards"] = flashcardsString
+    jsonString["curriculums"] = curriculumsString
+
+    return jsonString
