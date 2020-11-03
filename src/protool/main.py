3bfefee553249a0cbe6373c3cb4bdf08330a3317
cpp-maker <t.frankel@outlook.com>
2020-11-03 20:28:36 +0000

    now should be final commit

--- src/protool/main.py
+++ src/protool/main.py
@@ -210,19 +210,10 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("ProX")
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 52f7bf41dad40d9f6d32aa88bb562a32c86a011d
         #use json addoffline function
         if testOnline() == True: #update any offline changes to the database
             js.writeAll(user.user,user.offlineUser,curriculum.curriculums,curriculum.offlineCurriculums,task.tasks,task.offlineTasks,flash.flashcards,flash.offlineFlashcards)
 
-<<<<<<< HEAD
->>>>>>> 52f7bf41dad40d9f6d32aa88bb562a32c86a011d
-=======
->>>>>>> 52f7bf41dad40d9f6d32aa88bb562a32c86a011d
     def initMenu(self): #set up the menu bar, with File, syllabi and leaderboard
         self.filemenu = self.menu.addMenu("&File")
         self.syllabimenu = self.menu.addMenu("&Curriculums")
